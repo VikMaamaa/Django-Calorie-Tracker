4fe665f9aa17d99b3ad93196376f36246035c09b
vikmaamaa <maamaavictor@yahoo.com>
2023-03-08 22:23:36 +0100

    feat: adding consume Model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Food(models.Model):
@@ -11,3 +12,9 @@ class Food(models.Model):
     protein  = models.FloatField()
     fats = models.FloatField()
     calories = models.IntegerField()
+    
+
+class Consume(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed =  models.ForeignKey(Food, on_delete=models.CASCADE)   
