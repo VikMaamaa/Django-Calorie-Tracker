d64612a0add080e8fa534f33184bb933b38e1bbd
vikmaamaa <maamaavictor@yahoo.com>
2023-03-09 15:35:31 +0100

    feat: chart integration  and designing delete functionality

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Food, Consume
 
 # Create your views here.
@@ -18,3 +18,10 @@ def index(request):
     if(request.user != ''):
             consumed_food = Consume.objects.filter(user=request.user)    
     return render(request, 'myapp/index.html', {'foods': foods, 'consumed_food': consumed_food})
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == "POST":
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'myapp/delete.html')
